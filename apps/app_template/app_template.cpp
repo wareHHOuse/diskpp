08f937d0033796eaad3e596a5a8d99c9e3afe0a2
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2017-01-10 18:54:31 +0100

    template tutorial application

---
+++ apps/app_template/app_template.cpp
@@ -0,0 +1,171 @@
+/*
+ *       /\
+ *      /__\       Matteo Cicuttin (C) 2016 - matteo.cicuttin@enpc.fr
+ *     /_\/_\      École Nationale des Ponts et Chaussées - CERMICS
+ *    /\    /\
+ *   /__\  /__\    DISK++, a template library for DIscontinuous SKeletal
+ *  /_\/_\/_\/_\   methods.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * If you use this code for scientific publications, you are required to
+ * cite it.
+ */
+
+#include <iostream>
+#include <regex>
+
+#include "loaders/loader.hpp"
+#include "hho/hho.hpp"
+
+
+template<typename MeshType>
+void
+process_mesh(const MeshType& msh)
+{
+    for (auto& cl : msh)
+        std::cout << cl << std::endl;
+}
+
+
+int main(int argc, char **argv)
+{
+    using RealType = double;
+    
+    char    *filename       = nullptr;
+    int     elems_1d        = 8;
+    int ch;
+    
+
+    if (argc == 1)
+    {
+        std::cout << "Mesh format: 1D uniform" << std::endl;
+        
+        typedef disk::generic_mesh<RealType, 1>  mesh_type;
+        
+        mesh_type msh;
+        disk::uniform_mesh_loader<RealType, 1> loader(0,1,elems_1d);
+        loader.populate_mesh(msh);
+        
+        process_mesh(msh);
+        
+        return 0;
+    }
+    
+    filename = argv[1];
+    
+    if (std::regex_match(filename, std::regex(".*\\.typ1$") ))
+    {
+        std::cout << "Guessed mesh format: FVCA5 2D" << std::endl;
+        
+        typedef disk::generic_mesh<RealType, 2>  mesh_type;
+        
+        mesh_type msh;
+        disk::fvca5_mesh_loader<RealType, 2> loader;
+        if (!loader.read_mesh(filename))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            return 1;
+        }
+        loader.populate_mesh(msh);
+        
+        process_mesh(msh);
+    }
+    
+    if (std::regex_match(filename, std::regex(".*\\.mesh2d$") ))
+    {
+        std::cout << "Guessed mesh format: Netgen 2D" << std::endl;
+        
+        typedef disk::simplicial_mesh<RealType, 2>  mesh_type;
+        
+        mesh_type msh;
+        disk::netgen_mesh_loader<RealType, 2> loader;
+        if (!loader.read_mesh(filename))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            return 1;
+        }
+        loader.populate_mesh(msh);
+        
+        process_mesh(msh);
+    }
+    
+    if (std::regex_match(filename, std::regex(".*\\.msh$") ))
+    {
+        std::cout << "Guessed mesh format: FVCA6 3D" << std::endl;
+        
+        typedef disk::generic_mesh<RealType, 3>   mesh_type;
+        
+        mesh_type msh;
+        disk::fvca6_mesh_loader<RealType, 3> loader;
+        
+        
+        if (!loader.read_mesh(filename))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            return 1;
+        }
+        
+        loader.populate_mesh(msh);
+        
+        process_mesh(msh);
+    }
+    
+    if (std::regex_match(filename, std::regex(".*\\.mesh$") ))
+    {
+        std::cout << "Guessed mesh format: Netgen 3D" << std::endl;
+        
+        typedef disk::simplicial_mesh<RealType, 3>   mesh_type;
+        
+        mesh_type msh;
+        disk::netgen_mesh_loader<RealType, 3> loader;
+        if (!loader.read_mesh(filename))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            return 1;
+        }
+        loader.populate_mesh(msh);
+        
+        process_mesh(msh);
+    }
+    
+    if (std::regex_match(filename, std::regex(".*\\.quad$") ))
+    {
+        std::cout << "Guessed mesh format: Cartesian 2D" << std::endl;
+        
+        typedef disk::cartesian_mesh<RealType, 2>   mesh_type;
+        
+        mesh_type msh;
+        disk::cartesian_mesh_loader<RealType, 2> loader;
+        if (!loader.read_mesh(filename))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            return 1;
+        }
+        loader.populate_mesh(msh);
+        
+        process_mesh(msh);
+    }
+    
+    if (std::regex_match(filename, std::regex(".*\\.hex$") ))
+    {
+        std::cout << "Guessed mesh format: Cartesian 3D" << std::endl;
+        
+        typedef disk::cartesian_mesh<RealType, 3>   mesh_type;
+        
+        mesh_type msh;
+        disk::cartesian_mesh_loader<RealType, 3> loader;
+        if (!loader.read_mesh(filename))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            return 1;
+        }
+        loader.populate_mesh(msh);
+        
+        process_mesh(msh);
+    }
+    
+    return 0;
+}
