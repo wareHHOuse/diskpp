14f8784cf0f49bdb858ca665ca0ffe033c096bd2
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2017-01-10 14:17:13 +0100

    solvercost moved to the hho-misc repo

--- misc/solvercost/makemat.sh
+++
@@ -1,8 +0,0 @@
-
-for deg in $(seq 0 4); do
-    for sz in 2 4 8 16 32; do
-        ./diffusion -k $deg ../../../diskpp/meshes/3D_hexa/diskpp/testmesh-$sz-$sz-$sz.hex
-        mv matrix.txt ../../../diskpp/misc/solvercost/data/matrix-3D-k$deg-$sz-$sz-$sz.txt
-        mv rhs.txt ../../../diskpp/misc/solvercost/data/rhs-3D-k$deg-$sz-$sz-$sz.txt
-    done
-done
