527f96bfa864343bceac76cbd5f5389845d1d853
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2017-01-24 14:06:39 +0100

    multiscale stuff

---
+++ apps/multiscale/multiscale.cpp
@@ -0,0 +1,49 @@
+/*
+ *       /\
+ *      /__\       Matteo Cicuttin (C) 2016, 2017 - matteo.cicuttin@enpc.fr
+ *     /_\/_\      École Nationale des Ponts et Chaussées - CERMICS
+ *    /\    /\
+ *   /__\  /__\    DISK++, a template library for DIscontinuous SKeletal
+ *  /_\/_\/_\/_\   methods.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * If you use this code for scientific publications, you are required to
+ * cite it.
+ */
+
+#include <iostream>
+#include <regex>
+#include <unistd.h>
+#include <sstream>
+#include <iomanip>
+
+#include <map>
+
+#include "colormanip.h"
+
+#include "../../config.h"
+
+#include "loaders/loader.hpp"
+#include "hho/hho.hpp"
+
+
+
+int main(int argc, char **argv)
+{
+    using RealType = double;
+
+    if ( !std::regex_match(argv[1], std::regex(".*\\.mesh2d$") ) )
+    {
+        std::cout << "Mesh format must be Netgen 2D" << std::endl;
+        return 1;
+    }
+
+    auto msh = disk::load_netgen_2d_mesh<RealType>(argv[1]);
+
+    disk::multiscale_local_problem<decltype(msh)> mlp(1);
+
+    return 0;
+}
