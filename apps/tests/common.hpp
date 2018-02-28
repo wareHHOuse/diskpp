fcf60e9715790c1c24f76b7a4ebacbf6de668ffc
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2018-02-28 20:31:58 +0100

    Reorganized testing code. Committing to test code on the cluster.

---
+++ apps/tests/common.hpp
@@ -0,0 +1,192 @@
+/*
+ *       /\         DISK++, a template library for DIscontinuous SKeletal
+ *      /__\        methods.
+ *     /_\/_\
+ *    /\    /\      Matteo Cicuttin (C) 2016, 2017, 2018
+ *   /__\  /__\     matteo.cicuttin@enpc.fr
+ *  /_\/_\/_\/_\    École Nationale des Ponts et Chaussées - CERMICS
+ *
+ * This file is copyright of the following authors:
+ * Matteo Cicuttin (C) 2016, 2017, 2018         matteo.cicuttin@enpc.fr
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * If you use this code or parts of it for scientific publications, you
+ * are required to cite it as following:
+ *
+ * Implementation of Discontinuous Skeletal methods on arbitrary-dimensional,
+ * polytopal meshes using generic programming.
+ * M. Cicuttin, D. A. Di Pietro, A. Ern.
+ * Journal of Computational and Applied Mathematics.
+ * DOI: 10.1016/j.cam.2017.09.017
+ */
+
+#pragma once
+
+#include "core/loaders/loader.hpp"
+
+const size_t MIN_TEST_DEGREE = 0;
+const size_t MAX_TEST_DEGREE = 5;
+
+/*****************************************************************************************/
+template<typename Mesh>
+struct scalar_testing_function;
+
+template<template<typename, size_t, typename> class Mesh, typename T, typename Storage>
+struct scalar_testing_function< Mesh<T,2,Storage> >
+{
+	typedef Mesh<T,2,Storage> 				mesh_type;
+    typedef typename mesh_type::scalar_type scalar_type;
+    typedef typename mesh_type::point_type  point_type;
+
+    scalar_type operator()(const point_type& pt) const
+    {
+    	return std::sin(2. * M_PI * pt.x()) * std::sin(2. * M_PI * pt.y());
+    }
+};
+
+template<typename Mesh>
+auto make_scalar_testing_data(const Mesh& msh)
+{
+	return scalar_testing_function<Mesh>();
+}
+
+/*****************************************************************************************/
+template<typename Mesh>
+struct vector_testing_function;
+
+template<template<typename, size_t, typename> class Mesh, typename T, typename Storage>
+struct vector_testing_function< Mesh<T,2,Storage> >
+{
+	typedef Mesh<T,2,Storage> 				mesh_type;
+    typedef typename mesh_type::scalar_type scalar_type;
+    typedef typename mesh_type::point_type  point_type;
+    typedef Matrix<scalar_type, 2, 1> 		ret_type;
+
+    ret_type operator()(const point_type& pt) const
+    {
+    	ret_type ret;
+        ret(0) = std::sin(2. * M_PI * pt.x());
+        ret(1) = std::sin(2. * M_PI * pt.y());
+        return ret;
+    }
+};
+
+template<typename Mesh>
+auto make_vector_testing_data(const Mesh& msh)
+{
+	return vector_testing_function<Mesh>();
+}
+
+/*****************************************************************************************/
+
+template<typename T>
+std::vector< disk::generic_mesh<T, 2> >
+get_triangle_generic_meshes(void)
+{
+	std::vector<std::string> meshfiles;
+    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_1.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_2.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_3.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_4.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_5.typ1");
+
+
+    std::vector< disk::generic_mesh<T, 2> > ret;
+    for (size_t i = 0; i < meshfiles.size(); i++)
+    {
+        typedef disk::generic_mesh<T, 2>  mesh_type;
+
+        mesh_type msh;
+        disk::fvca5_mesh_loader<T, 2> loader;
+
+        if (!loader.read_mesh(meshfiles.at(i)))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            continue;
+        }
+        loader.populate_mesh(msh);
+
+        ret.push_back(msh);
+    }
+
+    return ret;
+}
+
+template<typename T>
+std::vector< disk::generic_mesh<T, 2> >
+get_quad_generic_meshes(void)
+{
+	std::vector<std::string> meshfiles;
+    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_1.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_2.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_3.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_4.typ1");
+    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_5.typ1");
+
+
+    std::vector< disk::generic_mesh<T, 2> > ret;
+    for (size_t i = 0; i < meshfiles.size(); i++)
+    {
+        typedef disk::generic_mesh<T, 2>  mesh_type;
+
+        mesh_type msh;
+        disk::fvca5_mesh_loader<T, 2> loader;
+
+        if (!loader.read_mesh(meshfiles.at(i)))
+        {
+            std::cout << "Problem loading mesh." << std::endl;
+            continue;
+        }
+        loader.populate_mesh(msh);
+
+        ret.push_back(msh);
+    }
+
+    return ret;
+}
+
+template<typename Mesh, typename Function>
+void
+do_testing(std::vector<Mesh>& meshes, const Function& run_test)
+{
+	using T = typename Mesh::scalar_type;
+
+	for (size_t k = MIN_TEST_DEGREE; k <= MAX_TEST_DEGREE; k++)
+    {
+        std::cout << "DEGREE " << k << std::endl;
+
+        std::vector<T> mesh_hs;
+        std::vector<T> l2_errors;
+
+        for(auto& msh : meshes)
+        {
+            auto error = run_test(msh, k);
+            mesh_hs.push_back( disk::mesh_h(msh) );
+            l2_errors.push_back(error);
+        }
+
+        for (size_t i = 0; i < mesh_hs.size(); i++)
+        {
+            if (i == 0)
+            {
+                std::cout << "    ";
+                std::cout << std::scientific << std::setprecision(5) << mesh_hs.at(i) << "    ";
+                std::cout << std::scientific << std::setprecision(5) << l2_errors.at(i);
+                std::cout << "     -- " << std::endl;
+            }
+            else
+            {
+                auto rate = std::log( l2_errors.at(i)/l2_errors.at(i-1) ) /
+                            std::log( mesh_hs.at(i)/mesh_hs.at(i-1) );
+                std::cout << "    ";
+                std::cout << std::scientific << std::setprecision(5) << mesh_hs.at(i) << "    ";
+                std::cout << std::scientific << std::setprecision(5) << l2_errors.at(i) << "    ";
+                std::cout << std::defaultfloat << std::setprecision(3) << rate << std::endl;
+            }
+        }
+    }
+}
+
--- apps/tests/scalar_stabilization.cpp
+++ apps/tests/scalar_stabilization.cpp
@@ -36,106 +36,75 @@
 
 #include "core/loaders/loader.hpp"
 
+#include "common.hpp"
+
 
 template<typename Mesh>
-typename Mesh::scalar_type
-run_test(const Mesh& msh, size_t degree)
+struct test_functor
 {
-    typedef Mesh mesh_type;
-    typedef typename mesh_type::cell        cell_type;
-    typedef typename mesh_type::face        face_type;
-    typedef typename mesh_type::scalar_type scalar_type;
-    typedef typename mesh_type::point_type  point_type;
+    typename Mesh::scalar_type
+    operator()(const Mesh& msh, size_t degree) const
+    {
+        typedef Mesh mesh_type;
+        typedef typename mesh_type::cell        cell_type;
+        typedef typename mesh_type::face        face_type;
+        typedef typename mesh_type::scalar_type scalar_type;
+        typedef typename mesh_type::point_type  point_type;
 
 
-    auto f = [&](const point_type& pt) -> scalar_type {
-        return std::sin(2. * M_PI * pt.x()) * std::sin(2. * M_PI * pt.y());
-    };
+        auto f = make_scalar_testing_data(msh);
 
-    typename revolution::hho_degree_info hdi(degree);
+        typename revolution::hho_degree_info hdi(degree);
 
-    scalar_type error = 0.0;
-    for (auto& cl : msh)
-    {   
-        auto gr = revolution::make_hho_scalar_laplacian(msh, cl, hdi);
-        auto stab = revolution::make_hho_fancy_stabilization(msh, cl, gr.first, hdi);
+        scalar_type error = 0.0;
+        for (auto& cl : msh)
+        {   
+            auto gr = revolution::make_hho_scalar_laplacian(msh, cl, hdi);
+            auto stab = revolution::make_hho_fancy_stabilization(msh, cl, gr.first, hdi);
 
-        size_t rec_size = revolution::scalar_basis_size(hdi.reconstruction_degree(), Mesh::dimension);
+            size_t rec_size = revolution::scalar_basis_size(hdi.reconstruction_degree(), Mesh::dimension);
 
-        Matrix<scalar_type, Dynamic, 1> proj = revolution::project_function(msh, cl, hdi, f);
+            Matrix<scalar_type, Dynamic, 1> proj = revolution::project_function(msh, cl, hdi, f);
 
-        error += proj.dot(stab*proj);
+            error += proj.dot(stab*proj);
+        }
+
+        return std::sqrt(error);
     }
+};
 
-    return std::sqrt(error);
+
+template<typename Mesh>
+test_functor<Mesh>
+get_test_functor(const std::vector<Mesh>& meshes)
+{
+    return test_functor<Mesh>();
 }
 
 
-int main(void)
+void test_triangles(void)
 {
     using T = double;
 
-    std::vector<std::string> meshfiles;
+    auto meshes = get_triangle_generic_meshes<T>();
+    auto tf = get_test_functor(meshes);
+    do_testing(meshes, tf);
+}
+
+void test_quads(void)
+{
+    using T = double;
+
+    auto meshes = get_quad_generic_meshes<T>();
+    auto tf = get_test_functor(meshes);
+    do_testing(meshes, tf);
+}
+
+int main(void)
+{
     
-    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_1.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_2.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_3.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_4.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_triangles/fvca5/mesh1_5.typ1");
-    
-    /*
-    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_1.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_2.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_3.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_4.typ1");
-    meshfiles.push_back("../../../diskpp/meshes/2D_quads/fvca5/mesh2_5.typ1");
-    */
-
-    for (size_t k = 0; k < 5; k++)
-    {
-        std::cout << "DEGREE " << k << std::endl;
-
-        std::vector<T> mesh_hs;
-        std::vector<T> l2_errors;
-
-        for (size_t i = 0; i < meshfiles.size(); i++)
-        {
-            typedef disk::generic_mesh<T, 2>  mesh_type;
-
-            mesh_type msh;
-            disk::fvca5_mesh_loader<T, 2> loader;
-            if (!loader.read_mesh(meshfiles.at(i)))
-            {
-                std::cout << "Problem loading mesh." << std::endl;
-                continue;
-            }
-            loader.populate_mesh(msh);
-
-            auto error = run_test(msh, k);
-            mesh_hs.push_back( disk::mesh_h(msh) );
-            l2_errors.push_back(error);
-        }
-
-        for (size_t i = 0; i < mesh_hs.size(); i++)
-        {
-            if (i == 0)
-            {
-                std::cout << "    ";
-                std::cout << std::scientific << std::setprecision(5) << mesh_hs.at(i) << "    ";
-                std::cout << std::scientific << std::setprecision(5) << l2_errors.at(i);
-                std::cout << "     -- " << std::endl;
-            }
-            else
-            {
-                auto rate = std::log( l2_errors.at(i)/l2_errors.at(i-1) ) /
-                            std::log( mesh_hs.at(i)/mesh_hs.at(i-1) );
-                std::cout << "    ";
-                std::cout << std::scientific << std::setprecision(5) << mesh_hs.at(i) << "    ";
-                std::cout << std::scientific << std::setprecision(5) << l2_errors.at(i) << "    ";
-                std::cout << std::defaultfloat << std::setprecision(3) << rate << std::endl;
-            }
-        }
-    }
+    test_triangles();
+    test_quads();
 
     return 0;
 }
