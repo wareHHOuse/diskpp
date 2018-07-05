wareHHOuse/diskpp

commit 1af1941b3d03943f85e811dc53c7503af191e215

Over-integration of simplicial elements
Be careful few convergence rates seem lower than expected but you have to increase the parameter di when you compute the L2-norms

core/quadratures/quad_bones.hpp
@@ -44,7 +44,7 @@ tetrahedron_quadrature(size_t degree)
     int dimension = 3;
 
 #ifdef USE_ARBQ
-    int rule = degree+1;
+    int rule = degree;
     if (rule == 0)
         rule = 1;
 #else
@@ -125,7 +125,7 @@ triangle_quadrature(size_t order)
     for(rule = 1; rule <= rule_num; rule++)
     {
         degree = dunavant_degree(rule);
-        if(degree > order)
+        if(degree >= order)
             break;
     }
     assert(rule != rule_num or degree >= order);

commit 641ceac9bbc3da848477848c16e302ce70c58c57

Commit 1af1941 broke integration, this could explain some problems in Karol's stuff that looked like underintegration.

core/quadratures/quad_bones.hpp
@@ -44,7 +44,7 @@ tetrahedron_quadrature(size_t degree)
     int dimension = 3;
 
 #ifdef USE_ARBQ
-    int rule = degree;
+    int rule = degree+1;
     if (rule == 0)
         rule = 1;
 #else
@@ -125,7 +125,7 @@ triangle_quadrature(size_t order)
     for(rule = 1; rule <= rule_num; rule++)
     {
         degree = dunavant_degree(rule);
-        if(degree >= order)
+        if(degree > order)
             break;
     }
     assert(rule != rule_num or degree >= order);