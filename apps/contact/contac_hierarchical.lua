75e11231dcdcec837d1afc4c3a6b386d337e0e61
KarolCascavita <Klcascavitam@unal.edu.co>
2018-07-13 00:09:38 +0200

    Contact with hierarchical meshes. Till now just fem working

---
+++ apps/contact/contac_hierarchical.lua
@@ -0,0 +1,24 @@
+config.degree_cell = 0
+config.degree_face = 0
+
+config.input_mesh = "../../../diskpp/meshes/2D_triangles/netgen/square_tri3.mesh2d"
+config.visit_output = "eigs_hho.silo"
+config.eigval_output = "eigs_hho.txt"
+
+hs.levels = 2
+hs.sol_level_min = 0
+hs.sol_level_max = 1
+
+hs.fem_theta = "p" -- Theta: p means 1; n means -1: z means 0
+hs.fem_gamma = 6 -- Gamma: 5 means 10^-5
+
+hs.hho_theta = "p" -- Theta: p means 1; n means -1: z means 0
+hs.hho_gamma = 5 -- Gamma: 5 means 10^-5
+
+
+-- Solver config for eigenvalue problems
+solver.feast.verbose = true -- Print solver logs
+solver.feast.tolerance = 8 -- Tolerance: 8 means 10^-8
+--solver.feast.min_eigval = 1 -- Minimum value for eigenvalue search range
+--solver.feast.max_eigval = 100 -- Maximum value for eigenvalue search range
+--solver.feast.subspace_size = 32 -- Subspace sizeit was just in c
