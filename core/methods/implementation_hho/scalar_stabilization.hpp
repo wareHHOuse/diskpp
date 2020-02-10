eafb2529d69d70d95b804cbf4481064ef159cd5c
npaster <nicolas.pignet@orange.fr>
2020-02-10 20:15:15 +0100

    first version to compute equilibrated fluxes

--- core/methods/implementation_hho/scalar_stabilization.hpp
+++ core/methods/implementation_hho/scalar_stabilization.hpp
@@ -406,4 +406,102 @@ make_scalar_hho_stabilization(const Mesh&                                       
     return make_scalar_hho_stabilization(msh, cl, reconstruction, cell_infos);
 }
 
+/**
+ * @brief compute the stabilization term \f$\sum_{F \in F_T} 1/h_F(u_F - u_T + \Pi^k_T R^{k+1}_T(\hat{u}_T) -
+ * R^{k+1}_T(\hat{u}_T), v_F - v_T + \Pi^k_T R^{k+1}_T(\hat{v}_T) - R^{k+1}_T(\hat{v}_T))_F \f$ for scalar HHO
+ * unknowns
+ *
+ * @tparam Mesh type of the mesh
+ * @param msh mesh
+ * @param cl cell
+ * @param reconstruction reconstruction operator \f$ R^{k+1}_T \f$
+ * @param cell_infos cell degree information
+ * @return dynamic_matrix<typename Mesh::coordinate_type> return the stabilization term
+ */
+template<typename Mesh>
+dynamic_matrix<typename Mesh::coordinate_type>
+make_scalar_hho_stabilization_fluxes(const Mesh&                                           msh,
+                                     const typename Mesh::cell_type&                       cl,
+                                     const dynamic_matrix<typename Mesh::coordinate_type>& reconstruction,
+                                     const CellDegreeInfo<Mesh>&                           cell_infos)
+{
+    using T = typename Mesh::coordinate_type;
+    typedef Matrix<T, Dynamic, Dynamic> matrix_type;
+
+    const matrix_type stab = make_scalar_hho_stabilization(msh, cl, reconstruction, cell_infos);
+
+    const auto celdeg = cell_infos.cell_degree();
+
+    const auto cbs = scalar_basis_size(celdeg, Mesh::dimension);
+
+    const auto cb = make_scalar_monomial_basis(msh, cl, celdeg);
+
+    const auto fcs            = faces(msh, cl);
+    const auto faces_infos    = cell_infos.facesDegreeInfo();
+    const auto num_faces_dofs = scalar_faces_dofs(msh, faces_infos);
+
+    matrix_type matA = matrix_type::Zero(num_faces_dofs, cbs + num_faces_dofs);
+
+    // Step 3: project on faces (eqn. 21)
+    size_t offset = cbs;
+    size_t row    = 0;
+    for (size_t face_i = 0; face_i < fcs.size(); face_i++)
+    {
+        const auto fdi = faces_infos[face_i];
+
+        if (fdi.hasUnknowns())
+        {
+            const auto fc = fcs[face_i];
+            const auto hf = diameter(msh, fc);
+
+            const auto facdeg = fdi.degree();
+            const auto fb     = make_scalar_monomial_basis(msh, fc, facdeg);
+            const auto fbs    = scalar_basis_size(facdeg, Mesh::dimension - 1);
+
+            matrix_type face_mass_matrix  = make_mass_matrix(msh, fc, fb);
+            matrix_type face_trace_matrix = matrix_type::Zero(fbs, cbs);
+
+            const auto face_quadpoints = integrate(msh, fc, celdeg + facdeg);
+            for (auto& qp : face_quadpoints)
+            {
+                const auto f_phi = fb.eval_functions(qp.point());
+                const auto c_phi = cb.eval_functions(qp.point());
+                face_trace_matrix += priv::outer_product(priv::inner_product(qp.weight(), f_phi), c_phi);
+            }
+
+            matA.block(row, offset, fbs, fbs) += face_mass_matrix/hf;
+            matA.block(row, 0, fbs, cbs) -= face_trace_matrix/hf;
+
+            offset += fbs;
+            row += fbs;
+        }
+    }
+
+    return (matA.transpose()).fullPivHouseholderQr().solve(stab);
+}
+
+/**
+ * @brief compute the stabilization term \f$\sum_{F \in F_T} 1/h_F(u_F - u_T + \Pi^k_T R^{k+1}_T(\hat{u}_T) -
+ * R^{k+1}_T(\hat{u}_T), v_F - v_T + \Pi^k_T R^{k+1}_T(\hat{v}_T) - R^{k+1}_T(\hat{v}_T))_F \f$ for scalar HHO
+ * unknowns
+ *
+ * @tparam Mesh type of the mesh
+ * @param msh mesh
+ * @param cl cell
+ * @param reconstruction reconstruction operator \f$ R^{k+1}_T \f$
+ * @param di hho degree information
+ * @return dynamic_matrix<typename Mesh::coordinate_type> return the stabilization term
+ */
+template<typename Mesh>
+dynamic_matrix<typename Mesh::coordinate_type>
+make_scalar_hho_stabilization_fluxes(const Mesh&                                           msh,
+                                     const typename Mesh::cell_type&                       cl,
+                                     const dynamic_matrix<typename Mesh::coordinate_type>& reconstruction,
+                                     const hho_degree_info&                                di)
+{
+    const CellDegreeInfo<Mesh> cell_infos(msh, cl, di.cell_degree(), di.face_degree(), di.grad_degree());
+
+    return make_scalar_hho_stabilization_fluxes(msh, cl, reconstruction, cell_infos);
+}
+
 } // end diskpp
