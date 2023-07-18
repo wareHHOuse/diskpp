627a5038626dee1f0cdad0daa986535aee56c6fd
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2023-07-18 17:13:47 +0200

    Rewrote tutorial 1.

--- apps/diskpp_tutorials/diskpp_tutorial_1.cpp
+++ apps/diskpp_tutorials/diskpp_tutorial_1.cpp
@@ -1,4 +1,14 @@
 /*
+ * DISK++, a template library for DIscontinuous SKeletal methods.
+ *
+ * Matteo Cicuttin (C) 2023
+ * matteo.cicuttin@polito.it
+ *
+ * Politecnico di Torino - DISMA
+ * Dipartimento di Matematica
+ */
+
+/*
  *       /\        Matteo Cicuttin (C) 2016-2020
  *      /__\       matteo.cicuttin@enpc.fr
  *     /_\/_\      École Nationale des Ponts et Chaussées - CERMICS
@@ -20,50 +30,147 @@
  * DOI: 10.1016/j.cam.2017.09.017
  */
 
-/* This DiSk++ tutorial shows how to iterate on the elements of a mesh
- * previously loaded.
+/* DiSk++ tutorial: create a mesh on the unit square/unit cube and iterate
+ * on its elements. Print some properties of the elements.
  */
 
-
-
 #include <iostream>
+#include <unistd.h>
 
 // For the mesh data structure
 #include "diskpp/mesh/mesh.hpp"
+// For the unit square/cube mesh generators
+#include "diskpp/mesh/meshgen.hpp"
 
-// For the loaders and related helper functions
-#include "diskpp/loaders/loader.hpp"
+enum class element_type {
+    triangle,
+    hexagon,
+    tetrahedron,
+};
 
-using disk::cells;
-using disk::faces;
-
-int main(int argc, const char **argv)
+/* This function is generic and will work for any kind of mesh */
+template<typename Mesh>
+void
+print_element_properties(const Mesh& msh)
 {
-    if (argc < 2)
+    for (auto& cl : msh)
     {
-        std::cout << "Please specify a DiSk++ cartesian mesh" << std::endl;
-        return 1;
+        std::cout << cl << std::endl;
+        std::cout << "  Diameter: " << diameter(msh, cl) << std::endl;
+        std::cout << "  Measure: " << measure(msh, cl) << std::endl;
+        std::cout << "  Barycenter: " << barycenter(msh, cl) << std::endl;
+        auto fcs = faces(msh, cl);
+        for (auto& fc : fcs)
+        {
+            std::cout << "  " <<fc << std::endl;
+            std::cout << "    Diameter: " << diameter(msh, fc) << std::endl;
+            std::cout << "    Measure: " << measure(msh, fc) << std::endl;
+            std::cout << "    Barycenter: " << barycenter(msh, fc) << std::endl;
+        }
+    }
+}
+
+/* If your function is appropriate only for 1D, 2D or 3D meshes, you
+ * can constrain the mesh type via the appropriate concept */
+template<disk::mesh_2D Mesh>
+void
+print_space_dimension(const Mesh& msh)
+{
+    std::cout << "This is a 2D mesh" << std::endl;
+}
+
+template<disk::mesh_3D Mesh>
+void
+print_space_dimension(const Mesh& msh)
+{
+    std::cout << "This is a 3D mesh" << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    int num_refs = 0;
+    element_type elem_type = element_type::triangle;
+
+    /* Parse the command line arguments */
+    int ch;
+    while ( (ch = getopt(argc, argv, "r:thT")) != -1 )
+    {
+        switch(ch)
+        {
+            case 'r':
+                num_refs = std::stoi(optarg);
+                if (num_refs < 0)
+                    num_refs = 0;
+                break;
+
+            case 't':
+                elem_type = element_type::triangle;
+                break;
+
+            case 'h':
+                elem_type = element_type::hexagon;
+                break;
+
+            case 'T':
+                elem_type = element_type::tetrahedron;
+                break;
+
+            case '?':
+            default:
+                std::cout << "Invalid option" << std::endl;
+                return 1;
+        }
     }
 
-    /* We will work on a 2D cartesian mesh; you can change this as you like*/
+    /* Coordinate type is double */
     using T = double;
-    disk::cartesian_mesh<T, 2> msh;
+    
+    switch (elem_type)
+    {
+        case element_type::triangle: {
+            /* The mesh type is a 2D simplicial mesh */
+            using mesh_type = disk::simplicial_mesh<T,2>;
+            /* Declare the mesh object... */
+            mesh_type msh;
+            /* ...and instantiate a mesh generator over it */
+            auto mesher = disk::make_simple_mesher(msh);
+            /* Refine the mesh */
+            for (auto nr = 0; nr < num_refs; nr++)
+                mesher.refine();
+            /* Call the code. */
+            print_space_dimension(msh);
+            print_element_properties(msh);
+        } break;
 
-    /* Load the mesh */
-    bool success = disk::load_mesh_diskpp_cartesian<T>(argv[1], msh);
-    if (!success)
-        return 1;
+        case element_type::hexagon: {
+            /* The mesh type is a 2D polygonal mesh */
+            using mesh_type = disk::generic_mesh<T,2>;
+            /* Declare the mesh object... */
+            mesh_type msh;
+            /* ...and instantiate a mesh generator over it */
+            auto mesher = disk::make_fvca5_hex_mesher(msh);
+            /* Make the specified level of the mesh */
+            mesher.make_level(num_refs);
+            /* Call the code. */
+            print_space_dimension(msh);
+            print_element_properties(msh);
+        } break;
 
-    /* Here cells(), faces() and boundary_faces() return a lightweight
-     * proxy object providing the appropriate begin() and end() methods. */
-
-    /* Iterate on the mesh cells */
-    for (auto& cl : cells(msh))
-        std::cout << cl << std::endl;
-
-    /* iterate on faces */
-    for (auto& fc : faces(msh))
-        std::cout << fc << std::endl;
+        case element_type::tetrahedron: {
+            /* The mesh type is a 3D simplicial mesh */
+            using mesh_type = disk::simplicial_mesh<T,3>;
+            /* Declare the mesh object... */
+            mesh_type msh;
+            /* ...and instantiate a mesh generator over it */
+            auto mesher = disk::make_simple_mesher(msh);
+            /* Refine the mesh */
+            for (auto nr = 0; nr < num_refs; nr++)
+                mesher.refine();
+            /* Call the code. */
+            print_space_dimension(msh);
+            print_element_properties(msh);
+        } break;
+    }
 
     return 0;
 }
