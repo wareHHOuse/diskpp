ddbcd6954ba2eedfd91c2a8c804111dd47b3a4f2
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2024-10-15 18:29:43 +0200

    Multiple layer overlap.

--- apps/hho_dd/src/hhodd.cpp
+++ apps/hho_dd/src/hhodd.cpp
@@ -7,62 +7,83 @@
 
 template<typename Mesh>
 auto
-subdomains(const Mesh& msh)
+make_overlapping_subdomains(const Mesh& msh, size_t overlap_layers)
 {
-    std::set<size_t> sds;
+    /* What this should actually do is a BFS, but Disk++ is missing
+     * some infrastructure to do a proper BFS. Therefore this code is
+     * quite inefficient and sooner or later must disappear. */
+    using cell_type = typename Mesh::cell_type;
+
+    using cvi = typename std::vector<cell_type>::iterator;
+
+    auto cvf = connectivity_via_faces(msh);
+
+    /* Collect the cells in the various subdomains */
+    std::map<size_t, std::vector<cell_type>> subdomains;
     for (auto& cl : msh) {
         auto di = msh.domain_info(cl);
-        sds.insert(di.tag());
+        subdomains[di.tag()].push_back(cl);
+    }
+    /* the cells are stored ordered in msh, therefore each
+     * vector in the map is sorted too. this is necessary
+     * precondition for std::lower_bound() below. */
+
+    for (size_t ol = 0; ol < overlap_layers; ol++) {
+        std::map<size_t, std::vector<cell_type>> layer_cells;
+        /* Iterate on the currently identified subdomains */
+        for (auto& [tag, cells] : subdomains) {
+            /* for each cell */
+            for (auto& cl : cells) {
+                auto fcs = faces(msh, cl);
+                for (auto& fc : fcs) {
+                    /* for each neighbour */
+                    auto [neigh, valid] = cvf.neighbour_via(msh, cl, fc);
+                    if (!valid)
+                        continue;
+
+                    /* and check if it is already in the current subdomain */
+                    bool present = std::binary_search(cells.begin(), cells.end(), neigh);
+
+                    /* if not, save it */
+                    if (not present)
+                        layer_cells[tag].push_back(neigh);
+                }
+            }
+        }
+
+        /* move the found neighbours to the subdomain */
+        for (auto& [tag, cells] : layer_cells) {
+            std::sort(cells.begin(), cells.end());
+            auto last = std::unique(cells.begin(), cells.end());
+            cells.erase(last, cells.end());
+            subdomains[tag].insert(subdomains[tag].end(), cells.begin(), cells.end());
+            std::sort(subdomains[tag].begin(), subdomains[tag].end());
+        }
     }
 
-    return sds; 
+    return subdomains;
 }
 
 template<typename Mesh>
 void
 hhodd(const Mesh& msh)
 {
-    auto subdoms = subdomains(msh);
-    auto cvf = connectivity_via_faces(msh);
-
-    std::map<size_t, std::vector<double>> cellmaps;
-    for (auto& sd : subdoms) {
-        cellmaps[sd].resize(msh.cells_size());
-    }
-
-    size_t clnum = 0;
-    for (auto& cl : msh) {
-        auto di = msh.domain_info(cl);
-        cellmaps[di.tag()][clnum] = 1.0;
-
-        auto fcs = faces(msh, cl);
-        for (auto& fc : fcs) {
-
-            auto bi = msh.boundary_info(fc);
-            if (!bi.is_boundary())
-                continue;
-            
-            auto [neigh, valid] = cvf.neighbour_via(msh, cl, fc);
-            if (!valid)
-                continue;
-
-            auto nofs = offset(msh, neigh);
-            cellmaps[di.tag()][nofs] = 1;
-        }
-
-        clnum++;
-    }
-
+    auto subdomains = make_overlapping_subdomains(msh, 5);
+    
 
     disk::silo_database silo;
     silo.create("hhodd.silo");
     silo.add_mesh(msh, "mesh");
 
-    for (auto& [tag, cellmap] : cellmaps)
+    for (auto& [tag, cells] : subdomains)
     {
+        std::vector<double> yesno(msh.cells_size());
+        for (auto& cl : cells)
+            yesno[offset(msh, cl)] += 1.0;
+
         std::stringstream ss;
         ss << "domain" << tag;
-        silo.add_variable("mesh", ss.str(), cellmap, disk::zonal_variable_t);
+        silo.add_variable("mesh", ss.str(), yesno, disk::zonal_variable_t);
     }
 
     silo.close();
