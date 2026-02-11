d9141323dad56e381c778f9c6e3acff39897e30f
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2026-02-11 18:57:12 +0100

    forgot file.

---
+++ apps/hho_dd/src/test_1d.cpp
@@ -0,0 +1,57 @@
+/*
+ * DISK++, a template library for DIscontinuous SKeletal methods.
+ *
+ * Matteo Cicuttin (C) 2026
+ * matteo.cicuttin@polito.it
+ *
+ * Politecnico di Torino - DISMA
+ * Dipartimento di Matematica
+ */
+
+#include <cstddef>
+#include <iostream>
+#include <regex>
+#include <set>
+#include <string>
+#include <filesystem>
+
+#include "diskpp/common/util.h"
+#include "diskpp/loaders/loader.hpp"
+#include "diskpp/loaders/loader_gmsh.hpp"
+#include "diskpp/loaders/loader_utils.hpp"
+#include "diskpp/mesh/meshgen.hpp"
+#include "diskpp/output/silo.hpp"
+#include "diskpp/methods/hho_slapl.hpp"
+#include "diskpp/methods/hho"
+#include "diskpp/methods/dg"
+#include "rasdd.hpp"
+#include "common.hpp"
+#include "solvers.hpp"
+#include "diskpp_git_revision.h"
+
+
+int main(void)
+{
+    using T = double;
+
+    disk::generic_mesh<T,1> msh;
+    disk::uniform_mesh_loader<T,1> loader(0,1,20);
+    loader.populate_mesh(msh);
+
+    std::vector<double> data;
+    double i = 0.0;
+    for (auto& cl : msh) {
+        std::cout << cl << std::endl;
+        data.push_back(i++);
+    }
+
+    disk::silo_database db;
+    db.create("test_1d.silo");
+    db.add_mesh(msh, "srcmesh");
+    db.add_mesh(msh, "dstmesh");
+    hho_diffusion_solver(msh, 4, db);
+    dg_diffusion_solver(msh, 5, 10.0, db);
+
+
+    return 0;
+}
